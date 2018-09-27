fc5451658bd2e9d8030b6299bea0d5c36017d8a9
Krzysztof Kaczor <chris@kaczor.io>
2018-09-27 22:53:11 +0800

    util function to prepare smartcontact, add hierarchy to integration smart contracts

--- test/integration.spec.ts
+++ test/integration.spec.ts
@@ -8,15 +8,20 @@ import {
   compareTransactionsWithReferentialImpl,
 } from "./helpers/compareWithReferentialImpl";
 
+function loadContract(fullName: string): { bin: string; abi: any; wrapper: any } {
+  const bin = fs.readFileSync(join(__dirname, "./contracts-compiled/", `${fullName}.bin`), "utf-8");
+  const abi = JSON.parse(fs.readFileSync(join(__dirname, "./contracts-compiled", `${fullName}.abi`), "utf-8"));
+  const wrapper = web3.eth.contract(abi).at("0x0");
+
+  return { bin, abi, wrapper };
+}
+
 describe("EMV-TS", () => {
   it("should work", () => compareWithReferentialImpl("60606040523415600e"));
 
   it("should work with more complicated bytecode", () => {
-    const bin = fs.readFileSync(join(__dirname, "./contracts-compiled/DumbContract.bin"), "utf-8");
-    const abi = JSON.parse(fs.readFileSync(join(__dirname, "./contracts-compiled/DumbContract.abi"), "utf-8"));
-    const contract = web3.eth.contract(abi).at("0x0");
-
-    const callTestFunctionData: string = contract.test.getData().slice(2);
+    const { bin, wrapper } = loadContract("simple/Simple");
+    const callTestFunctionData: string = wrapper.test.getData().slice(2);
 
     return compareTransactionsWithReferentialImpl(async vm => {
       const deploymentResult = await vm.runTx({ data: bin });
